282bae8dd4855d0b24aab8747e8bdac3b2f6d8ea
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2020-10-19 17:07:10 +0300

    feat: solve five task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  // throw new CustomError('Not implemented');
+
+  const cats = "^^";
+
+  const newArr = matrix.reduce((acum, elem) => acum.concat(elem), []);
+
+  let count = 0;
+
+  newArr.forEach((elem) => {
+    if (elem === cats) {
+      count += 1;
+    }
+  });
+
+  return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    const dreamteam = members.filter((elem) => typeof elem === "string");
+    return dreamteam
+      .reduce(
+        (final, current) =>
+          final.concat(current.trim().toUpperCase().slice(0, 1)),
+        []
+      )
+      .sort()
+      .join("");
+  } else {
+    return false;
+  }
 };
